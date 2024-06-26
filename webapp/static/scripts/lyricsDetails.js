1d859738c47c16b76e4a68549069a653160fcc2d
fyrodems <fyrodems@gmail.com>
2024-06-27 00:10:13 +0200

    create actions container for details page

--- webapp/static/scripts/lyricsDetails.js
+++ webapp/static/scripts/lyricsDetails.js
@@ -14,139 +14,137 @@
 //   }
 // });
 
-  function loadLyricsDetailsFromDetails(lyricsId) {
-    db.collection("lyrics")
-      .doc(lyricsId)
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          var lyricsData = doc.data();
-          $("#lyricsTitle").text(lyricsData.title);
-          $("#lyricsContent").text(lyricsData.lyrics);
-          $("#lyricsRating").text(`Rating: ${lyricsData.rating}`);
-          $("#lyricsFavorite").text(`Favorite: ${lyricsData.favorite}`);
-          $("#lyricsCreatedAt").text(
-            `Created At: ${new Date(
-              lyricsData.createdAt.toDate()
-            ).toISOString()}`
-          );
+function loadLyricsDetailsFromDetails(lyricsId) {
+  db.collection("lyrics")
+    .doc(lyricsId)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        var lyricsData = doc.data();
+        $("#lyricsTitle").text(lyricsData.title);
+        $("#lyricsContent").text(lyricsData.lyrics);
+        $("#lyricsRating").text(`Rating: ${lyricsData.rating}`);
+        $("#lyricsCreatedAt").text(
+          `${formatDateString(
+            new Date(lyricsData.createdAt.toDate()).toISOString()
+          )}`
+        );
 
-          var favoriteButton = $("#markAsFavoriteButton");
-          favoriteButton.html(
-            lyricsData.favorite
-              ? '<i class="fa fa-heart" style="font-size:20px; color: crimson; cursor: pointer;"></i>'
-              : '<i class="fa fa-heart-o" style="font-size:20px; cursor: pointer;"></i>'
-          );
-          favoriteButton.off("click").on("click", function () {
-            markAsFavoriteFromDetails(lyricsId, !lyricsData.favorite);
-          });
-        } else {
-          alert("No such lyrics found!");
-        }
-      })
-      .catch((error) => {
-        console.error("Error getting lyrics: ", error);
-      });
-  }
-
-  function saveLyricsFromDetails() {
-    var user = firebase.auth().currentUser;
-    if (!user) return;
-
-    var lyrics = $("#lyricsContent").text();
-    var title = $("#lyricsTitle").text();
-
-    if (title !== null) {
-      db.collection("lyrics")
-        .doc(lyricsId)
-        .update({
-          userId: user.uid,
-          title: title,
-          lyrics: lyrics,
-          translations: {},
-          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-          favorite: false,
-          rating: 0,
-        })
-        .then(() => {
-          alert("Lyrics saved successfully!");
-        })
-        .catch((error) => {
-          console.error("Error saving lyrics: ", error);
-        });
-    }
-  }
-
-  function markAsFavoriteFromDetails(docId, favoriteStatus) {
-    var user = firebase.auth().currentUser;
-    if (!user) return;
-
-    db.collection("lyrics")
-      .doc(docId)
-      .update({
-        favorite: favoriteStatus,
-      })
-      .then(() => {
-        loadLyricsDetailsFromDetails(docId);
-      })
-      .catch((error) => {
-        console.error("Error updating favorite status: ", error);
-      });
-  }
-
-  function favoriteHandler() {
-    db.collection("lyrics")
-      .doc(lyricsId)
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          var lyricsData = doc.data();
+        var favoriteButton = $("#markAsFavoriteButton");
+        favoriteButton.html(
+          lyricsData.favorite
+            ? '<i class="fa fa-heart" style="font-size:20px; color: crimson; cursor: pointer;"></i>'
+            : '<i class="fa fa-heart-o" style="font-size:20px; cursor: pointer;"></i>'
+        );
+        favoriteButton.off("click").on("click", function () {
           markAsFavoriteFromDetails(lyricsId, !lyricsData.favorite);
-        }
-      })
-      .catch((error) => {
-        console.error("Error getting document:", error);
-      });
-  }
+        });
+      } else {
+        alert("No such lyrics found!");
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting lyrics: ", error);
+    });
+}
 
-  function rateLyricsFromDetails() {
-    var user = firebase.auth().currentUser;
-    if (!user) return;
+function saveLyricsFromDetails() {
+  var user = firebase.auth().currentUser;
+  if (!user) return;
 
-    var rating = prompt("Rate this song (1-5):", "5");
-    rating = parseInt(rating);
-    if (isNaN(rating) || rating < 1 || rating > 5) {
-      alert("Invalid rating");
-      return;
-    }
+  var lyrics = $("#lyricsContent").text();
+  var title = $("#lyricsTitle").text();
 
+  if (title !== null) {
     db.collection("lyrics")
       .doc(lyricsId)
       .update({
-        rating: rating,
+        userId: user.uid,
+        title: title,
+        lyrics: lyrics,
+        translations: {},
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        favorite: false,
+        rating: 0,
       })
       .then(() => {
-        alert("Lyrics rated successfully!");
-        loadLyricsDetailsFromDetails(lyricsId);
+        alert("Lyrics saved successfully!");
       })
       .catch((error) => {
-        console.error("Error rating lyrics: ", error);
+        console.error("Error saving lyrics: ", error);
       });
   }
+}
 
-  function deleteLyricsFromDetails() {
-    var user = firebase.auth().currentUser;
-    if (!user) return;
+function markAsFavoriteFromDetails(docId, favoriteStatus) {
+  var user = firebase.auth().currentUser;
+  if (!user) return;
 
-    db.collection("lyrics")
-      .doc(lyricsId)
-      .delete()
-      .then(() => {
-        alert("Lyrics deleted successfully!");
-        window.location.href = "/generate_page";
-      })
-      .catch((error) => {
-        console.error("Error deleting lyrics: ", error);
-      });
+  db.collection("lyrics")
+    .doc(docId)
+    .update({
+      favorite: favoriteStatus,
+    })
+    .then(() => {
+      loadLyricsDetailsFromDetails(docId);
+    })
+    .catch((error) => {
+      console.error("Error updating favorite status: ", error);
+    });
+}
+
+function favoriteHandler() {
+  db.collection("lyrics")
+    .doc(lyricsId)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        var lyricsData = doc.data();
+        markAsFavoriteFromDetails(lyricsId, !lyricsData.favorite);
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting document:", error);
+    });
+}
+
+function rateLyricsFromDetails() {
+  var user = firebase.auth().currentUser;
+  if (!user) return;
+
+  var rating = prompt("Rate this song (1-5):", "5");
+  rating = parseInt(rating);
+  if (isNaN(rating) || rating < 1 || rating > 5) {
+    alert("Invalid rating");
+    return;
   }
 
+  db.collection("lyrics")
+    .doc(lyricsId)
+    .update({
+      rating: rating,
+    })
+    .then(() => {
+      alert("Lyrics rated successfully!");
+      loadLyricsDetailsFromDetails(lyricsId);
+    })
+    .catch((error) => {
+      console.error("Error rating lyrics: ", error);
+    });
+}
+
+function deleteLyricsFromDetails() {
+  var user = firebase.auth().currentUser;
+  if (!user) return;
+
+  db.collection("lyrics")
+    .doc(lyricsId)
+    .delete()
+    .then(() => {
+      alert("Lyrics deleted successfully!");
+      window.location.href = "/generate_page";
+    })
+    .catch((error) => {
+      console.error("Error deleting lyrics: ", error);
+    });
+}
--- webapp/static/scripts/utils.js
+++ webapp/static/scripts/utils.js
@@ -5,3 +5,14 @@ function openSideMenu() {
 function hideSideMenu() {
   $("#sideMenu").attr("class", "side-menu");
 }
+
+function formatDateString(isoString) {
+  const date = new Date(isoString);
+
+  // Extract the day, month, and year from the Date object
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // getMonth() is zero-based
+  const year = date.getFullYear();
+
+  return `${day}.${month}.${year}`;
+}
